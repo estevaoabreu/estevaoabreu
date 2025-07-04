03b91533882806f4fc2bc43cd5de63b6a7f14da5
Estêvão Abreu <estevao.abreu40@gmail.com>
2025-07-04 20:35:47 +0100

    stats

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-vercel';
+import { injectAnalytics } from '@vercel/analytics/sveltekit'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
