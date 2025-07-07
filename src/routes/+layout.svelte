9064c7bbfe7553bea67ae77cdf431111b380a3f7
Estêvão Abreu <estevao.abreu40@gmail.com>
2025-07-07 17:57:53 +0100

    speed analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,10 @@
 	import { injectAnalytics } from '@vercel/analytics/sveltekit';
 	import { dev } from '$app/environment';
 	import Footer from './footer.svelte';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
 	injectAnalytics({ mode: dev ? 'development' : 'production' });
+	injectSpeedInsights();
 </script>
 
 <slot />
